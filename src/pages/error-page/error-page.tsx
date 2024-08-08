db77a6d1ac330b24d501ac4ffac876311a7dce45
juaneliasjabib <juaneliasjabib02@gmail.com>
2024-08-08 08:13:57 -0500

    comment

--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -4,11 +4,11 @@ import { isErrorResponse } from './is-error-response';
 export default function ErrorPage() {
 	const error = useRouteError();
 
-	// TODO Back after and make a 404 and return to home here
+	// TODO Back after and make a 404 and return to home here , not time.
 	return (
 		<div id='error-page'>
 			<h1>Oops!</h1>
-			<p>Sorry, an unexpected error has perro.</p>
+			<p>Sorry, an unexpected error has happen.</p>
 			<p>
 				<i>
 					{isErrorResponse(error)
